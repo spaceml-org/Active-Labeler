3525564b323e8a0371a55fea499f361da40179b7
subhiksha-m <47918614+subhiksha-m@users.noreply.github.com>
2021-09-25 14:48:07 +0800

    added comments

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from argparse import ArgumentParser
 from pipeline import Pipeline
 
 def main():
+    """" Initializes a pipeline class object and runs pipeline.main """
+
     parser = ArgumentParser()
     parser.add_argument("--config_path", type=str, help="Path to config file")
     args = parser.parse_args()
-    config_path = args.config_path
+    config_path = args.config_path                      #get command line argument
 
-    # initialize pipeline object
-    pipeline = Pipeline(config_path=config_path)
-    pipeline.main()
+    pipeline = Pipeline(config_path=config_path)        #initialize pipeline object
+    pipeline.main()                                     #run pipeline.main
 
 
 if __name__ == '__main__':
