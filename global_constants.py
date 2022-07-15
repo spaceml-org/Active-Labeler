7f2a2682fe4acc0696fc9fbd6e42ee55c3205478
Tarun Narayanan <tarun@madstreetden.com>
2022-07-16 04:40:11 +0530

    dataset

--- global_constants.py
+++ global_constants.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 config_path = args.config_path
 
 config = load_config(config_path)
-dataset_name = config["data"]["dataset_name"]
+dataset_name = config["data"].get("dataset_name", None)
 
 LABELED_DIR = f"{dataset_name}/labeled"
 VAL_DIR = f"{dataset_name}/validation"
